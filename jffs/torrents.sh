9c5ab151d23e9e87f5fc6e3244b2af2c0e12e50c
groot <groot@groot-mbp>
2024-01-06 15:38:51 +0200

    Update torrents.sh

--- jffs/torrents.sh
+++ jffs/torrents.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 SERVICE="/jffs/tg.sh"
+SERVICE1="/jffs/smb.sh"
+CRON_PATH=$(cat /var/spool/cron/crontabs/parrot | wc -l)
 
 mv /tmp/mnt/Transcend/Download2/Complete/* /tmp/mnt/Transcend/xStorage/xTorrents/
+
+if [ "${CRON_PATH}" -eq "1" ] ; then
+   bash $SERVICE1
+fi
 
 ps | grep -v grep | grep $SERVICE > /dev/null
 result=$?
