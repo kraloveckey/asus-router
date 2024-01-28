472a425e7220b325a85dfeee5e8f06da14aa7fc8
kraloveckey <kraloveckey@groot-mbp>
2024-01-28 20:23:06 +0200

    Update torrents.sh script.

--- jffs/torrents.sh
+++ jffs/torrents.sh
@@ -2,18 +2,18 @@
 
 SERVICE="/jffs/tg.sh"
 SERVICE1="/jffs/smb.sh"
-CRON_PATH=$(cat /var/spool/cron/crontabs/parrot | wc -l)
+IPTABLES_PATH=$(iptables -L -n -v | grep 14887 | wc -l)
+
+if [ "${IPTABLES_PATH}" -eq "0" ] ; then
+   bash ${SERVICE1}
+fi
+
+ps | grep -v grep | grep ${SERVICE} > /dev/null
+RESULT=$?
+if [ "${RESULT}" -eq "1" ] ; then
+   bash ${SERVICE} &
+fi
 
 mv /tmp/mnt/Transcend/Download2/Complete/* /tmp/mnt/Transcend/xStorage/xTorrents/
-
-if [ "${CRON_PATH}" -eq "1" ] ; then
-   bash $SERVICE1
-fi
-
-ps | grep -v grep | grep $SERVICE > /dev/null
-result=$?
-if [ "${result}" -eq "1" ] ; then
-   bash $SERVICE &
-fi
 
 exit 0
